7358ac9ddf39807c8f5a5178c766aa754437e2f3
darkness071104 <fauzanramadhanputra@students.undip.ac.id>
2024-01-11 14:46:22 +0700

    fix login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,54 +6,70 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         width: double.infinity, // Make the container fill the width
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(15.0),
         color: Colors.blue, // Replace with the exact color code from the design
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch, // Add this line
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Image.asset(
               'assets/logo.png',
               width: 300,
               height: 100,
             ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              child: Text(
-                'Login as Student',
-                style: TextStyle(fontSize: 18), // Set the font size here
+            SizedBox(height: 15),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
-              onPressed: () {
-                // Handle the button action for student login
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // Button color
-                  onPrimary: Colors.blue, // Text color
-                  minimumSize: Size(
-                      double.infinity, 60) // Set the minimum size of the butt
-                  ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text(
-                'Login as Mentor',
-                style: TextStyle(fontSize: 18), // Set the font size here
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: Text(
+                        'Login as Student',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      onPressed: () {
+                        // Handle the button action for student login
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                        minimumSize: Size(double.infinity, 60),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    ElevatedButton(
+                      child: Text(
+                        'Login as Mentor',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      onPressed: () {
+                        // Handle the button action for mentor login
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                        minimumSize: Size(double.infinity, 60),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {
-                // Handle the button action for mentor login
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // Button color
-                  onPrimary: Colors.blue, // Text color
-                  minimumSize: Size(
-                      double.infinity, 60) // Set the minimum size of the button
-                  ),
             ),
             SizedBox(height: 10),
             TextButton(
               child: Text(
                 "Don't have an account?",
-                style: TextStyle(color: Colors.white, fontSize: 16),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.white,
+                ),
               ),
               onPressed: () {
                 // Handle the button action for account creation
